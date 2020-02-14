784ec42db85f9f058203006880dc4b4e860861e3
akashsrivastava-php <akashsrvstv34@gmail.com>
2020-02-14 14:14:27 +0530

    Updates

---
+++ precache-manifest.e6109e58a662f2f0592e73dc79aa2907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf7667e3e8f64fa86a53",
+    "url": "/starwar/static/css/main.b653114b.chunk.css"
+  },
+  {
+    "revision": "cf7667e3e8f64fa86a53",
+    "url": "/starwar/static/js/main.2d39afca.chunk.js"
+  },
+  {
+    "revision": "aa8b2ecc524677b44afa",
+    "url": "/starwar/static/js/runtime~main.c6f57be2.js"
+  },
+  {
+    "revision": "b2dc7d10f5496a6209e1",
+    "url": "/starwar/static/css/2.08cb41d0.chunk.css"
+  },
+  {
+    "revision": "b2dc7d10f5496a6209e1",
+    "url": "/starwar/static/js/2.92929038.chunk.js"
+  },
+  {
+    "revision": "d58a602b0c5517010312461708850804",
+    "url": "/starwar/index.html"
+  }
+];
